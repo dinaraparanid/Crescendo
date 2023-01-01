8b5294f5b9cd173fcf8bec4695f08b6aba3a9e63
dinaraparanid <dinaraparanid@gmail.com>
2023-01-01 23:36:32 +0300

    stream service done

--- app/src/main/java/com/paranid5/mediastreamer/StreamService.kt
+++ app/src/main/java/com/paranid5/mediastreamer/StreamService.kt
@@ -6,7 +6,6 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.graphics.drawable.Icon
-import android.media.AudioManager
 import android.media.session.MediaController
 import android.media.session.MediaSession
 import android.media.session.MediaSessionManager
@@ -14,19 +13,22 @@ import android.media.session.PlaybackState
 import android.os.Binder
 import android.os.Build
 import android.os.SystemClock
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.core.graphics.drawable.IconCompat
-import androidx.media3.common.AudioAttributes
+import androidx.media3.common.*
 import androidx.media3.common.C.*
-import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import com.paranid5.mediastreamer.utils.extensions.toBitmap
+import kotlinx.coroutines.*
+import java.util.concurrent.atomic.AtomicLong
 
-class StreamService : Service() {
+class StreamService : Service(), CoroutineScope by MainScope() {
     companion object {
         private const val NOTIFICATION_ID = 101
         private const val STREAM_CHANNEL_ID = "stream_channel"
+        private const val PLAYBACK_UPDATE_COOLDOWN = 5000L
 
         const val Broadcast_PAUSE = "com.paranid5.mediastreamer.StreamService.PAUSE"
         const val Broadcast_RESUME = "com.paranid5.mediastreamer.StreamService.RESUME"
@@ -36,28 +38,34 @@ class StreamService : Service() {
 
         const val ACTION_PAUSE = "pause"
         const val ACTION_RESUME = "resume"
-        const val ACTION_SEEK_BACK = "move_back"
-        const val ACTION_SEEK_FORWARD = "move_forward"
+        const val ACTION_10_SECS_BACK = "back"
+        const val ACTION_10_SECS_FORWARD = "forward"
+        const val ACTION_ADD_TO_FAVOURITE = "add_to_favourite"
+        const val ACTION_REMOVE_FROM_FAVOURITE = "remove_from_favourite"
         const val ACTION_DISMISS = "dismiss"
     }
 
     sealed class Actions(val requestCode: Int) {
-        object Pause : Actions(100)
-        object Resume : Actions(101)
-        object SeekBack : Actions(102)
-        object SeekForward : Actions(103)
-        object Dismiss : Actions(104)
+        object Pause : Actions(NOTIFICATION_ID + 1)
+        object Resume : Actions(NOTIFICATION_ID + 2)
+        object TenSecsBack : Actions(NOTIFICATION_ID + 3)
+        object TenSecsForward : Actions(NOTIFICATION_ID + 4)
+        object AddToFavourite : Actions(NOTIFICATION_ID + 5)
+        object RemoveFromFavourite : Actions(NOTIFICATION_ID + 6)
+        object Dismiss : Actions(NOTIFICATION_ID + 7)
     }
 
     private inline val Actions.playbackIntent: PendingIntent
         get() {
             val playbackAction = Intent(this@StreamService, StreamService::class.java).also {
                 it.action = when (this) {
-                    Actions.Resume -> ACTION_RESUME             // Resume
-                    Actions.Pause -> ACTION_PAUSE               // Pause
-                    Actions.SeekBack -> ACTION_SEEK_BACK        // Seek to 10 secs earlier
-                    Actions.SeekForward -> ACTION_SEEK_FORWARD  // Seek to 10 secs later
-                    Actions.Dismiss -> ACTION_DISMISS           // Remove notification
+                    Actions.Resume -> ACTION_RESUME
+                    Actions.Pause -> ACTION_PAUSE
+                    Actions.TenSecsBack -> ACTION_10_SECS_BACK
+                    Actions.TenSecsForward -> ACTION_10_SECS_FORWARD
+                    Actions.AddToFavourite -> ACTION_ADD_TO_FAVOURITE
+                    Actions.RemoveFromFavourite -> ACTION_REMOVE_FROM_FAVOURITE
+                    Actions.Dismiss -> ACTION_DISMISS
                 }
             }
 
@@ -81,35 +89,23 @@ class StreamService : Service() {
 
     private val binder = object : Binder() {}
     private var url = ""
-    private var currentPlaybackPosition = 0L
+    private val currentPlaybackPosition = AtomicLong(0L)
+    private lateinit var playbackMonitorTask: Job
 
-    private var _currentMediaItem: MediaItem? = null
-    private val currentMediaItem
-        get() = _currentMediaItem!!
-
+    private lateinit var currentMediaItem: MediaItem
     private inline val currentMetadata
-        get() = _currentMediaItem!!.mediaMetadata
+        get() = currentMediaItem.mediaMetadata
 
-    private var _mediaSessionManager: MediaSessionManager? = null
-    private val mediaSessionManager
-        get() = _mediaSessionManager!!
+    // ----------------------- Media session management -----------------------
 
-    private var _mediaSession: MediaSession? = null
-    private val mediaSession
-        get() = _mediaSession!!
+    private lateinit var mediaSessionManager: MediaSessionManager
+    private lateinit var mediaSession: MediaSession
+    private lateinit var transportControls: MediaController.TransportControls
 
-    private var _transportControls: MediaController.TransportControls? = null
-    private val transportControls
-        get() = _transportControls!!
-
-    private var _audioManager: AudioManager? = null
-    private val audioManager
-        get() = _audioManager!!
-
-    private var _player: ExoPlayer? = ExoPlayer.Builder(applicationContext)
+    private val player = ExoPlayer.Builder(applicationContext)
         .setAudioAttributes(
             AudioAttributes.Builder()
-                .setContentType(AUDIO_CONTENT_TYPE_MUSIC)
+                .setContentType(AUDIO_CONTENT_TYPE_MOVIE)
                 .setUsage(USAGE_MEDIA)
                 .build(),
             true
@@ -117,12 +113,40 @@ class StreamService : Service() {
         .setHandleAudioBecomingNoisy(true)
         .setWakeMode(WAKE_MODE_NETWORK)
         .build()
-
-    private inline val player
-        get() = _player!!
+        .apply { addListener(playerStateChangedListener) }
 
     private inline val isPlaying
-        get() = _player!!.isPlaying
+        get() = player.isPlaying
+
+    private val playerStateChangedListener: Player.Listener = object : Player.Listener {
+        override fun onPlaybackStateChanged(playbackState: Int) {
+            super.onPlaybackStateChanged(playbackState)
+            if (playbackState == Player.STATE_IDLE)
+                restartPlayer()
+        }
+
+        override fun onIsPlayingChanged(isPlaying: Boolean) {
+            super.onIsPlayingChanged(isPlaying)
+
+            when {
+                isPlaying -> startPlaybackMonitoring()
+                else -> stopPlaybackMonitoring()
+            }
+
+            showNotification(isPlaying)
+        }
+
+        override fun onMediaMetadataChanged(mediaMetadata: MediaMetadata) {
+            super.onMediaMetadataChanged(mediaMetadata)
+            showNotification(isPlaying)
+        }
+
+        override fun onPlayerError(error: PlaybackException) {
+            super.onPlayerError(error)
+            showNotification(isPlaying = false)
+            Log.e("StreamService", "onPlayerError", error)
+        }
+    }
 
     private val pauseReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
@@ -155,13 +179,15 @@ class StreamService : Service() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
             createChannel()
 
+        initMediaSession()
         playNewStream(intent)
         return START_NOT_STICKY
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        releasePlayer()
+        releaseMedia()
+        removeNotification()
 
         unregisterReceiver(pauseReceiver)
         unregisterReceiver(resumeReceiver)
@@ -183,28 +209,36 @@ class StreamService : Service() {
                 }
             )
 
-    private fun pausePlayback() = player.pause()
+    private fun pausePlayback() {
+        currentPlaybackPosition.set(player.currentPosition)
+        player.pause()
+    }
 
     private fun resumePlayback() {
         player.playWhenReady = true
     }
 
-    private fun playNewStream(intent: Intent) {
-        url = intent.getStringExtra(URL_ARG)!!
-        val mediaItem = MediaItem.fromUri(url)
+    private fun playNewStream(newUrl: String) {
+        url = newUrl
+        currentMediaItem = MediaItem.fromUri(url)
 
         player.run {
-            setMediaItem(mediaItem)
+            setMediaItem(this@StreamService.currentMediaItem)
             playWhenReady = true
             prepare()
         }
-
-        buildNotification()
     }
 
-    private fun releasePlayer() {
-        _player?.release()
-        _player = null
+    private fun playNewStream(intent: Intent) =
+        playNewStream(intent.getStringExtra(URL_ARG)!!)
+
+    private fun restartPlayer() = playNewStream(url)
+
+    private fun releaseMedia() {
+        player.stop()
+        player.release()
+        mediaSession.release()
+        transportControls.stop()
     }
 
     private inline val newMediaSessionCallback
@@ -239,16 +273,16 @@ class StreamService : Service() {
             .setActions(playbackStateActions)
             .setState(
                 PlaybackState.STATE_PAUSED,
-                currentPlaybackPosition,
+                currentPlaybackPosition.get(),
                 1.0F,
                 SystemClock.elapsedRealtime()
             )
             .build()
 
     private fun initMediaSession() {
-        _mediaSessionManager = getSystemService(MEDIA_SESSION_SERVICE)!! as MediaSessionManager
-        _mediaSession = MediaSession(applicationContext, "StreamService")
-        _transportControls = mediaSession.controller.transportControls
+        mediaSessionManager = getSystemService(MEDIA_SESSION_SERVICE)!! as MediaSessionManager
+        mediaSession = MediaSession(applicationContext, "StreamService")
+        transportControls = mediaSession.controller.transportControls
 
         mediaSession.run {
             isActive = true
@@ -293,12 +327,12 @@ class StreamService : Service() {
         )
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun Notification.Builder.setActions() = this
+    private fun Notification.Builder.setActions(isPlaying: Boolean) = this
         .addAction(
             Notification.Action.Builder(
                 Icon.createWithResource("", R.drawable.prev_track),
-                ACTION_SEEK_BACK,
-                Actions.SeekBack.playbackIntent
+                ACTION_10_SECS_BACK,
+                Actions.TenSecsBack.playbackIntent
             ).build()
         )
         .addAction(
@@ -319,17 +353,17 @@ class StreamService : Service() {
         .addAction(
             Notification.Action.Builder(
                 Icon.createWithResource("", R.drawable.next_track),
-                ACTION_SEEK_FORWARD,
-                Actions.SeekForward.playbackIntent
+                ACTION_10_SECS_FORWARD,
+                Actions.TenSecsForward.playbackIntent
             ).build()
         )
 
-    private inline val notificationBuilderOreo
-        @RequiresApi(Build.VERSION_CODES.O)
-        get() = Notification
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun getNotificationBuilderOreo(isPlaying: Boolean) =
+        Notification
             .Builder(applicationContext, STREAM_CHANNEL_ID)
             .setContent()
-            .setActions()
+            .setActions(isPlaying)
 
     // --------------------------- Notification Compat ---------------------------
 
@@ -354,12 +388,12 @@ class StreamService : Service() {
             )
         )
 
-    private fun NotificationCompat.Builder.setActions() = this
+    private fun NotificationCompat.Builder.setActions(isPlaying: Boolean) = this
         .addAction(
             NotificationCompat.Action.Builder(
                 IconCompat.createWithResource(this@StreamService, R.drawable.prev_track),
-                ACTION_SEEK_BACK,
-                Actions.SeekBack.playbackIntent
+                ACTION_10_SECS_BACK,
+                Actions.TenSecsBack.playbackIntent
             ).build()
         )
         .addAction(
@@ -380,26 +414,44 @@ class StreamService : Service() {
         .addAction(
             NotificationCompat.Action.Builder(
                 IconCompat.createWithResource(this@StreamService, R.drawable.next_track),
-                ACTION_SEEK_FORWARD,
-                Actions.SeekForward.playbackIntent
+                ACTION_10_SECS_FORWARD,
+                Actions.TenSecsForward.playbackIntent
             ).build()
         )
 
-    private inline val notificationBuilderCompat
-        get() = NotificationCompat
+    private fun getNotificationBuilderCompat(isPlaying: Boolean) =
+        NotificationCompat
             .Builder(applicationContext, STREAM_CHANNEL_ID)
             .setContent()
-            .setActions()
+            .setActions(isPlaying)
+    
+    // --------------------------- Handle Notification ---------------------------
 
-    private fun buildNotification() {
+    private fun showNotification(isPlaying: Boolean) = startForeground(
+        NOTIFICATION_ID,
         when {
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.O -> notificationBuilderOreo.build()
-            else -> notificationBuilderCompat.build()
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ->
+                getNotificationBuilderOreo(isPlaying).build()
+            else -> getNotificationBuilderCompat(isPlaying).build()
         }
-    }
+    )
 
     private fun removeNotification() = when {
         Build.VERSION.SDK_INT >= Build.VERSION_CODES.N -> stopForeground(STOP_FOREGROUND_REMOVE)
         else -> stopForeground(true)
     }
+
+    // --------------------------- Playback Monitoring ---------------------------
+
+    private fun startPlaybackMonitoring() {
+        playbackMonitorTask = launch { 
+            while (true) {
+                delay(PLAYBACK_UPDATE_COOLDOWN)
+                currentPlaybackPosition.set(player.currentPosition)
+                // TODO: save position
+            }
+        }
+    }
+
+    private fun stopPlaybackMonitoring() = playbackMonitorTask.cancel()
 }
