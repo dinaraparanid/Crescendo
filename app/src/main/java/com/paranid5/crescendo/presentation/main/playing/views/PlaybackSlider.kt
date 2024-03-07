f0d85bb27c2653ee9d11adfa023ec4dbd2dde7b9
dinaraparanid <dinaraparanid@gmail.com>
2024-03-07 23:21:49 +0300

    google fucked up with compose 1.2 - drag and drop in slider

--- app/src/main/java/com/paranid5/crescendo/presentation/main/playing/views/PlaybackSlider.kt
+++ app/src/main/java/com/paranid5/crescendo/presentation/main/playing/views/PlaybackSlider.kt
@@ -5,8 +5,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Slider
-import androidx.compose.material3.SliderDefaults
+import androidx.compose.material.Slider
+import androidx.compose.material.SliderDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
