83a31ebc7a81bcdc918b5e3b82fa5430ab785b6b
dinaraparanid <dinaraparanid@gmail.com>
2024-07-24 23:05:34 +0300

    di test check

---
+++ app/src/test/kotlin/com/paranid5/crescendo/DITest.kt
@@ -0,0 +1,27 @@
+package com.paranid5.crescendo
+
+import androidx.lifecycle.SavedStateHandle
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.engine.HttpClientEngine
+import org.koin.test.verify.verify
+import kotlin.test.Test
+
+class DITest {
+    @Test
+    fun koinGraphTest() {
+        appModule.verify(
+            extraTypes = listOf(
+                Unit::class,
+                Set::class,
+                Boolean::class,
+                kotlin.Function0::class,
+                android.content.Context::class,
+                android.app.Application::class,
+                kotlinx.serialization.StringFormat::class,
+                HttpClientEngine::class,
+                HttpClientConfig::class,
+                SavedStateHandle::class,
+            )
+        )
+    }
+}
